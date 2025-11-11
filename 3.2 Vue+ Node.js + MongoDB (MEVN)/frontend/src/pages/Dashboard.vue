cd0e7cb7cf1105f9957d6161e8b65b9c12bc32dc
Isuru Shehara <isurushehara@gmail.com>
2025-11-11 08:28:57 +0530

    feat(frontend): Create dashboard page

---
+++ 3.2 Vue+ Node.js + MongoDB (MEVN)/frontend/src/pages/Dashboard.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { api } from "../api";
+import { auth } from "../store/auth";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const message = ref("");
+
+onMounted(async () => {
+  try {
+    const token = auth.getToken();
+
+    const res = await api.get("/api/protected", {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    message.value = res.data.message;
+  } catch (err) {
+    alert("Session expired");
+    auth.logout();
+    router.push("/login");
+  }
+});
+</script>
+
+<template>
+  <div>
+    <h2>Dashboard</h2>
+    <p>{{ message }}</p>
+    <button @click="auth.logout(); $router.push('/login')">Logout</button>
+  </div>
+</template>

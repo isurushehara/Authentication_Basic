0dc85408f57df4f9f02abefedbd7e1b0cb52bd33
Isuru Shehara <isurushehara@gmail.com>
2025-11-12 19:02:27 +0530

    feat(database): Create db file

---
+++ 3.3 Vue+ Node.js + MongoDB (MEVN)/backend/db.js
@@ -0,0 +1,12 @@
+// backend/db.js
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export async function connectDB() {
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log('✅ MongoDB connected');
+}
